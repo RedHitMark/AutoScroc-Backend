adf6734b22285c6da991444b545302265bd22eaf
RedHitMark <russodivito.marco@gmail.com>
2020-02-05 19:09:59 +0100

    -query

--- api/v1.0/rentManagement.js
+++ api/v1.0/rentManagement.js
@@ -21,8 +21,10 @@ async function getRents(idPage) {
     return new Promise((resolve, reject) => {
         const db = new Database();
 
-        const sql = "SELECT Rent.licensePlate, Rent.matriculationYear, Rent.price, Rent.km, Cars.name, Cars.carType, Cars.engineType, Cars.doors, Cars.trasmission, Cars.hp, Cars.kw, Cars.torque, Cars.cc, Cars.numCylinders, Cars.cylindersType, Cars.topSpeed, Cars.acc, Cars.weight, Cars.img  FROM Rent LEFT JOIN Cars ON Rent.idCar=Cars.id LIMIT 10 OFFSET ?;";
-        const value = [(idPage - 1) * 10];
+        const PAGE_LENGHT = 10;
+
+        const sql = "SELECT Rent.licensePlate, Rent.matriculationYear, Rent.price, Rent.km, Cars.name, Cars.carType, Cars.engineType, Cars.doors, Cars.trasmission, Cars.hp, Cars.kw, Cars.torque, Cars.cc, Cars.numCylinders, Cars.cylindersType, Cars.topSpeed, Cars.acc, Cars.weight, Cars.img  FROM Rent LEFT JOIN Cars ON Rent.idCar=Cars.id LIMIT ? OFFSET ?;";
+        const value = [PAGE_LENGHT, (idPage - 1) * PAGE_LENGHT];
 
         db.readQuery(sql, value).then((result) => {
             resolve(result);
