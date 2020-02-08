ecc877a5ce415cd6971605af0455ca245dc3401b
RedHitMark <russodivito.marco@gmail.com>
2020-02-08 19:20:33 +0100

    explorer api

--- api/v1.0/rentManagement.js
+++ api/v1.0/rentManagement.js
@@ -6,7 +6,7 @@ async function getRents(idPage) {
 
         const PAGE_LENGHT = 20;
 
-        const sql = "SELECT Rent.licensePlate, Rent.matriculationYear, Rent.price, Rent.km, Cars.name, Cars.carType, Cars.engineType, Cars.doors, Cars.trasmission, Cars.hp, Cars.kw, Cars.torque, Cars.cc, Cars.numCylinders, Cars.cylindersType, Cars.topSpeed, Cars.acc, Cars.weight, Cars.img  FROM Rent LEFT JOIN Cars ON Rent.idCar=Cars.id WHERE Rent.type=0 LIMIT ? OFFSET ?;";
+        const sql = "SELECT Rent.licensePlate, Rent.matriculationYear, Rent.price, Rent.km, Cars.id, Cars.idModel, Cars.idBrand, Cars.name, Cars.carType, Cars.engineType, Cars.doors, Cars.trasmission, Cars.hp, Cars.kw, Cars.torque, Cars.cc, Cars.numCylinders, Cars.cylindersType, Cars.topSpeed, Cars.acc, Cars.weight, Cars.img  FROM Rent LEFT JOIN Cars ON Rent.idCar=Cars.id WHERE Rent.type=0 LIMIT ? OFFSET ?;";
         const value = [PAGE_LENGHT, (idPage - 1) * PAGE_LENGHT];
 
         db.readQuery(sql, value).then((result) => {
